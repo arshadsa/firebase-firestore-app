90a1152704667f829a2e25668032e266f8409bf9
Arshad <arshad.syed.anwar@gmail.com>
2022-10-08 00:03:27 +0100

    initially complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { db } from "./firebase.config";
-import { collection, getDocs, addDoc, doc, updateDoc } from "firebase/firestore";
+import { collection, getDocs, addDoc, doc, updateDoc,deleteDoc } from "firebase/firestore";
+import { async } from "@firebase/util";
 
 function App() {
   const [user, setUsers] = useState([]);
@@ -17,6 +18,10 @@ function App() {
     const userDoc = doc(db, "users", id);
     const Fields = {age: age+1};
     await updateDoc(userDoc, Fields);
+  }
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "users", id);
+    await deleteDoc(userDoc);
   }
   useEffect(() => {
     const getUsers = async () => {
@@ -38,6 +43,7 @@ function App() {
             <h1>Name: {user.name}</h1>
             <h1>Age: {user.age}</h1>
             <button onClick={()=>updateUser(user.id, user.age)}>Increase Age</button>
+            <button onClick={()=>deleteUser(user.id)}>Delete User</button>
           </div>
         );
       })}
